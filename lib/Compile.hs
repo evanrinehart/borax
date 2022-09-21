496455bcb391d35df1bae0426b0dc0883fca73b1
Evan Rinehart <evanrinehart@gmail.com>
2022-09-20 21:59:51 -0500

    Refactor compile to fail immediately on stray case statement.

--- lib/Compile.hs
+++ lib/Compile.hs
@@ -9,18 +9,18 @@ import Syntax
 import Data.IntMap as IM
 import Data.Map as M
 
-type Compile a = ReaderT (Map String Int) (State CompileData) a
+-- A local monad to compile a function (Reader + State + Exception)
+type Compile a = ReaderT (Map String Int) (StateT CompileData (Except (Int,String))) a
 
 data CompileData = CompileData
   { cdBreakable :: [Int]
   , cdLabelMap  :: Map String Int
   , cdSwitchTable :: Maybe [(Constant,Int)]
   , cdGenerator :: Int
-  , cdGraph :: IntMap (Int,Node)
-  , cdProblem :: Maybe String
+  , cdGraph :: CodeGraph
   } deriving Show
 
-data Node =
+data OpCode =
   Goto Int |
   IfGoto Expr Int Int |
   Switch Expr [(Constant,Int)] Int |
@@ -29,46 +29,50 @@ data Node =
   Null
     deriving Show
 
--- return a control node graph and the root node id
-compileStatement :: Statement -> Either String (Int, IntMap (Int,Node))
-compileStatement stmt = result where
-  g0 = IM.fromList [(0, (0, Null))]
-  cdata = CompileData [] M.empty Nothing 1 g0 Nothing
-  (n, cdata') = runState (runReaderT (compile stmt 0) finalLMap) cdata
-  CompileData _ finalLMap _ _ g mproblem = cdata'
-  result = case mproblem of
-    Nothing  -> Right (n,g)
-    Just msg -> Left msg
-    
+data Node = Node
+  { nodeSourceLine  :: Int
+  , nodeInstruction :: OpCode } deriving Show
 
--- compile a statement given the node ID of the following node
--- may add graph nodes, may add labels, may update internal switch map
--- may push or pop node ID for breaking out of current while or switch
+type CodeGraph = IntMap Node
+
+
+compileFunction :: Statement -> Either (Int,String) (Int,CodeGraph)
+compileFunction stmt = answer where
+  answer = fmap (\(n,CompileData _ _ _ _ g) -> (n,g)) algorithmResults
+  algorithmResults = runExcept (runStateT (runReaderT (compile stmt 0) finalLMap) blankData)
+  blankGraph = IM.fromList [(0, (Node 0 Null))]
+  blankData  = CompileData [] M.empty Nothing 1 blankGraph
+  -- Laziness Note
+  -- provides the computed table for gotos as a readable resource to the algorithm
+  ~(Right (_, CompileData _ finalLMap _ _ _)) = algorithmResults
+
+-- The main compilation loop. Traverse the statements and build a flow chart.
+-- If a case statement is encountered outside a switch statement, compilation fails.
 compile :: Statement -> Int -> Compile Int
 
 compile (AutoStatement _ _ sub) next = compile sub next
 compile (ExtrnStatement _ _ sub) next = compile sub next
-compile (RValueStatement line ex) next = addNode (line, Eval ex next)
-compile (ReturnStatement line (Just ex)) next = addNode (line, Return ex)
-compile (ReturnStatement line Nothing) next = addNode (line, Null)
+compile (RValueStatement line ex) next = addNode line (Eval ex next)
+compile (ReturnStatement line (Just ex)) next = addNode line (Return ex)
+compile (ReturnStatement line Nothing) next = addNode line (Null)
 compile (NullStatement line) next = return next
 
 compile (ConditionalStatement line ex body Nothing) next = do
   n1 <- compile body next
-  n <- addNode (line, IfGoto ex n1 next)
+  n <- addNode line (IfGoto ex n1 next)
   return n
 
 compile (ConditionalStatement line ex body1 (Just body2)) next = do
   n2 <- compile body2 next
   n1 <- compile body1 next
-  addNode (line, IfGoto ex n1 n2)
+  addNode line (IfGoto ex n1 n2)
 
 compile (WhileStatement line ex body) next = do  
   here <- generate
   pushBreakable next
   n1 <- compile body here
   popBreakable
-  addNodeWithId here (line, IfGoto ex n1 next)
+  addNodeWithId here (Node line (IfGoto ex n1 next))
   return here
 
 compile (SwitchStatement line ex body) next = do
@@ -78,26 +82,26 @@ compile (SwitchStatement line ex body) next = do
   popBreakable
   mnew <- swapSwitchTable old
   let Just new = mnew
-  addNode (line, Switch ex new next)
+  addNode line (Switch ex new next)
   
 compile (GotoStatement line ex) next = do
   let NameExpr name = ex
   finalLabelMap <- ask
   let target = finalLabelMap M.! name
-  addNode (line, Goto target)
+  addNode line (Goto target)
 
 compile (BreakStatement line) next = do
   n <- currentBreakTarget
-  addNode (line, Goto n)
+  addNode line (Goto n)
 
 compile (LabelStatement _ name sub) next = do
   n <- compile sub next
   addLabel name n
   return n
 
-compile (CaseStatement _ k sub) next = do
+compile (CaseStatement line k sub) next = do
   n <- compile sub next
-  addCase k n
+  addCase line k n
   return n
 
 compile (CompoundStatement _ [])     next = return next
@@ -123,28 +127,28 @@ swapSwitchTable sm' = do
   modify (\s -> s { cdSwitchTable = sm' })
   return sm
 
-getSwitchTable :: Compile [(Constant,Int)]
-getSwitchTable = do
+getSwitchTable :: Int -> Compile [(Constant,Int)]
+getSwitchTable lineNo = do
   mtable <- gets cdSwitchTable
   case mtable of
-    Nothing    -> assertProblem "no switch statement found" >> return []
+    Nothing    -> throwError (lineNo, "No switch statement found here")
     Just table -> return table
 
 addLabel :: String -> Int -> Compile ()
 addLabel name n = modify (\s -> s { cdLabelMap = M.insert name n (cdLabelMap s) })
 
-addNodeWithId :: Int -> (Int,Node) -> Compile ()
-addNodeWithId n p = modify (\s -> s { cdGraph = IM.insert n p (cdGraph s) })
+addNodeWithId :: Int -> Node -> Compile ()
+addNodeWithId n node = modify (\s -> s { cdGraph = IM.insert n node (cdGraph s) })
 
-addNode :: (Int, Node) -> Compile Int
-addNode p = do
+addNode :: Int -> OpCode -> Compile Int
+addNode line opcode = do
   n <- generate
-  addNodeWithId n p
+  addNodeWithId n (Node line opcode)
   return n
 
-addCase :: Constant -> Int -> Compile ()
-addCase k n = do
-  table <- getSwitchTable
+addCase :: Int -> Constant -> Int -> Compile ()
+addCase lineNo k n = do
+  table <- getSwitchTable lineNo
   modify (\s -> s { cdSwitchTable = Just ((k,n):table) })
 
 generate :: Compile Int
@@ -152,6 +156,3 @@ generate = do
   g <- gets cdGenerator
   modify (\s -> s { cdGenerator = g + 1 })
   return g
-
-assertProblem :: String -> Compile ()
-assertProblem msg = modify (\s -> s { cdProblem = Just msg })
